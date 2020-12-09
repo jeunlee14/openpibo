854f7487257a19215a8ebb3921210ac65c16fbdf
leeyunjai82 <leeyunjai@circul.us>
2020-12-09 19:40:37 +0900

    update

--- example/test/move.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('/home/pi/openpibo/lib')
-
-from motion.motionlib import cMotion
-
-m = cMotion()
-m.set_motion("wave1", 2)
-#m.set_motion("forward1", 1)
-#m.set_motion("greeting", 1)
