26c51f2adc7d4e1b674439fd05a6d0358a722d07
leeyunjai82 <leeyunjai@circul.us>
2021-01-15 09:32:42 +0900

    update

--- lib/speech/speechlib.py
+++ lib/speech/speechlib.py
@@ -22,21 +22,14 @@ class cSpeech:
   def __init__(self, conf=None):
     self.translator = google_translator()
     self.kakao_account = conf.KAKAO_ACCOUNT
-    #self.google_account = conf.GOOGLE_ACCOUNT
-    #if self.google_account:
-    #  os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.google_account
-    #  self.client = speech.SpeechClient()
-    #  self.sample_rate = 16000
 
   def translate(self, string, to='ko'):
     return self.translator.translate(string, lang_tgt=to)
 
   def tts(self, string, filename="tts.mp3", lang="ko"):
-    #gTTS(string, lang=lang).save(filename)
-    #os.system('espeak "{}" {} -w {}'.format(string, opt, filename))
     '''curl -v "https://kakaoi-newtone-openapi.kakao.com/v1/synthesize" \
     -H "Content-Type: application/xml" \
-    -H "Authorization: KakaoAK dabfb05367a032764b5688f1602643a5" \
+    -H "Authorization: KakaoAK API_KEY" \
     -d '<speak> 그는 그렇게 말했습니다.
     <voice name="MAN_DIALOG_BRIGHT">잘 지냈어? 나도 잘 지냈어.</voice>
     <voice name="WOMAN_DIALOG_BRIGHT" speechStyle="SS_ALT_FAST_1">금요일이 좋아요.</voice> </speak>' > result.mp3'''
@@ -50,31 +43,12 @@ class cSpeech:
       f.write(r.content)
 
   def stt(self, filename="stream.wav", lang="ko-KR"'''en-US''', timeout=5):
-    #if self.google_account == None:
-    #  return False
-
     cmd = "arecord -D dmic_sv -c2 -r 16000 -f S32_LE -d {} -t wav -q -vv -V streo stream.raw;sox stream.raw -c 1 -b 16 stream.wav;rm stream.raw".format(timeout)
     os.system(cmd)
-    # Loads the audio into memory
-    #with io.open(filename, 'rb') as audio_file:
-    #  content = audio_file.read()
-    #  audio = types.RecognitionAudio(content=content)
-
-    #config = types.RecognitionConfig(
-    #  encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-    #  sample_rate_hertz=self.sample_rate,
-    #  language_code=lang)
-
-    # Detects speech in the audio file
-    #response = self.client.recognize(config, audio)
-    #results = []
-    #for result in response.results:
-    #  results.append(result.alternatives[0].transcript)
-    #return results
 
     '''curl -v "https://kakaoi-newtone-openapi.kakao.com/v1/recognize" \
     -H "Transfer-Encoding: chunked" -H "Content-Type: application/octet-stream" \
-    -H "Authorization: KakaoAK dabfb05367a032764b5688f1602643a5" \
+    -H "Authorization: KakaoAK API_KEY" \
     --data-binary @stream.wav '''
 
     url = 'https://kakaoi-newtone-openapi.kakao.com/v1/recognize'
@@ -87,7 +61,6 @@ class cSpeech:
     res = requests.post(url, headers=headers, data=data)
     result_json_string = res.text[res.text.index('{"type":"finalResult"'):res.text.rindex('}')+1]
     result = json.loads(result_json_string)
-    #print(result)
     return result['value']
 
 class cDialog:
