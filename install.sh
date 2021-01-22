5bac840ab8563e7c1ea49a5383cac46edaf5bafa
leeyunjai82 <68631024+leeyunjai82@users.noreply.github.com>
2021-01-22 17:01:41 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ sudo apt install tesseract-ocr tesseract-ocr-kor -y
 sudo apt install curl openjdk-8-jdk -y
 sudo pip3 install -r requirements.txt
 bash <(curl -s https://raw.githubusercontent.com/konlpy/konlpy/master/scripts/mecab.sh)
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
+rm wiringpi-latest.deb
 sudo systemctl disable hciuart.service
 cd lib/servo;make;sudo make install;make clean
 cd ../../utils/mic
