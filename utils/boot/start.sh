35c4fd68178292e84b2ad2fd92b21c4c770fbf34
leeyunjai82 <leeyunjai@circul.us>
2021-02-20 16:20:39 +0900

    update

--- utils/boot/start.sh
+++ utils/boot/start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-EIP=$(ifconfig eth0 | grep "inet " | awk '{print $2}') || true
-SSID=$(iw wlan0 info | grep ssid | awk '{print $2}') || true
-WIP=$(ifconfig wlan0 | grep "inet " | awk '{print $2}') || true
+E0=$(ifconfig eth0 2>/dev/null | grep "inet " | awk '{print $2}') || true
+E1=$(ifconfig eth1 2>/dev/null| grep "inet " | awk '{print $2}') || true
+W0=$(ifconfig wlan0 2>/dev/null| grep "inet " | awk '{print $2}') || true
+if [ "$W0" = "" ];then
+  S0=""	
+else
+  S0=$(iw wlan0 info | grep ssid | awk '{print $2}') || true
+fi
+W1=$(ifconfig wlan1 2>/dev/null| grep "inet " | awk '{print $2}') || true
+if [ "$W1" = "" ];then
+  S1=""	
+else
+  S1=$(iw wlan0 info | grep ssid | awk '{print $2}') || true
+fi
 
-if [ "$EIP" = "" ];then
-  EIP="-"
-fi
-if [ "$SSID" = "" ];then
-  SSID="-"
-fi
-if [ "$WIP" = "" ];then
-  WIP="-"
-fi
-
-python3 /home/pi/openpibo/utils/boot/net_info.py --eip $EIP --wip $WIP --ssid $SSID
+python3 /home/pi/openpibo/utils/boot/net_info.py --eip "$E0,$E1" --wip "$W0,$S0,$W1,$S1" 
