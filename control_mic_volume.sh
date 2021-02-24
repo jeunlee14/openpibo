20c7ceb1dce99edbf7e5f99e5d9c85a7de7ff016
leeyunjai82 <leeyunjai@circul.us>
2021-02-24 17:29:00 +0900

    update

--- control_mic_volume.sh
+++ control_mic_volume.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # sudo alsamixer --> F6 --> snd_rpi_i2s_card --> F5 
-amixer -c2 sset Boost 196
+arecord -D dmic_sv -c2 -r 44100 -f S32_LE -d 1 -t wav -V mono -v file.wav
+amixer -c1 sset Boost 196
