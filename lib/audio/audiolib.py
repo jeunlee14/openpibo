179e4ce7a11c634c0d72b614caf0aab867fba30f
leeyunjai82 <leeyunjai@circul.us>
2021-02-24 18:43:58 +0900

    update

--- lib/audio/audiolib.py
+++ lib/audio/audiolib.py
@@ -4,8 +4,11 @@ class cAudio:
   # out: local/hdmi/both
   # volume: mdB
   # filename: mp3/wav
-  def play(self, filename, out='local', volume='-2000'):
-    os.system("omxplayer -o {} --vol {} {} &".format(out, volume, filename))
+  def play(self, filename, out='local', volume='-2000', background=True):
+    if background:
+      os.system("omxplayer -o {} --vol {} {} &".format(out, volume, filename))
+    else:
+      os.system("omxplayer -o {} --vol {} {}".format(out, volume, filename))
 
   def stop(self):
     os.system('sudo pkill omxplayer')
