0f45d2e5c999762a4e2ffd733b41ae85d454d3bd
leeyunjai82 <leeyunjai@circul.us>
2021-03-11 16:35:17 +0900

    update

--- control_mic_volume.sh
+++ control_mic_volume.sh
@@ -2,4 +2,11 @@
 
 # sudo alsamixer --> F6 --> snd_rpi_i2s_card --> F5 
 arecord -D dmic_sv -c2 -r 44100 -f S32_LE -d 1 -t wav -V mono -v file.wav
-amixer -c1 sset Boost 196
+rm file.wav
+
+cn=`arecord -l | grep sndrpii2scard`
+cn="${cn:5:1}"
+
+echo -e "\n\n ## I2S CARD Number is " $cn
+#amixer -c $cn sset Boost 196
+amixer -c $cn sset Boost 77%
