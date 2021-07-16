cbdc15213db853cb2037b99876be29228c8f2cf9
leeyunjai82 <leeyunjai@circul.us>
2021-07-16 19:16:59 +0900

    update

--- lib/speech/speechlib.py
+++ lib/speech/speechlib.py
@@ -5,7 +5,7 @@ import json
 import os
 from konlpy.tag import Mecab
 import requests
-from google_trans_new import google_translator
+from speech.google_trans_new import google_translator
 #from google.cloud import speech
 #from google.cloud.speech import enums
 #from google.cloud.speech import types
@@ -24,6 +24,27 @@ class cSpeech:
     self.kakao_account = conf.KAKAO_ACCOUNT
 
   def translate(self, string, to='ko'):
+    '''curl -v -X POST "https://dapi.kakao.com/v2/translation/translate" \
+    -d "src_lang=kr" \
+    -d "target_lang=en" \
+    --data-urlencode "query=지난해 3월 오픈한 카카오톡 주문하기는 현재까지 약 250만명의 회원을 확보했으며, 주문 가능한 프랜차이즈 브랜드는 38개, 가맹점수는 약 1만 5천여곳에 달한다. 전 국민에게 친숙한 카카오톡 UI를 활용하기 때문에 남녀노소 누구나 쉽게 이용할 수 있으며, 별도의 앱을 설치할 필요 없이 카카오톡 내에서 모든 과정이 이뤄지는 것이 특징이다. 지난해 9월 업계 최초로 날짜와 시간을 예약한 뒤 설정한 매장에서 주문 음식을 찾아가는 ‘픽업’ 기능을 도입했고, 올해 1월 스마트스피커 ‘카카오미니’에서 음성을 통해 주문 가능한 메뉴를 안내받을 수 있도록 서비스를 연동하며 차별화를 꾀했다. 중소사업자들이 카카오톡 주문하기에 입점하게 되면 4,300만 카카오톡 이용자들과의 접점을 확보하고, 간편한 주문 과정으로 만족도를 높일 수 있게 된다. 카카오톡 메시지를 통해 신메뉴 출시, 프로모션 등의 소식을 전달할 수 있고, 일대일 채팅 기능을 적용하면 고객과 직접 상담도 가능하다." \
+    -H "Authorization: KakaoAK {REST_API_KEY}"'''
+
+    '''
+    # kakao translate source
+    url = 'https://dapi.kakao.com/v2/translation/translate'
+    headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'KakaoAK ' + self.kakao_account
+    }
+
+    res = requests.post(url, headers=headers, data={"src_lang":"kr", "target_lang":"en", "query":string})
+    try:
+      result = {"result":True, "value":json.loads(res.text)["translated_text"]}
+
+    except Exception as ex:
+      result = {"result":False, "value":""}
+    return result['value']'''
     return self.translator.translate(string, lang_tgt=to)
 
   def tts(self, string, filename="tts.mp3", lang="ko"):
