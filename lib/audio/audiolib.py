d0bc44ebb9d109623e3bc5e0a4bfdcea2879e792
Jinpyo-Hong <hojp7874@circul.us>
2021-07-26 12:26:17 +0900

    Update audiolib mute기능 추가

--- lib/audio/audiolib.py
+++ lib/audio/audiolib.py
@@ -1,9 +1,18 @@
 import os
 
+HIGH = 1
+LOW = 0
+
 class cAudio:
   # out: local/hdmi/both
   # volume: mdB
   # filename: mp3/wav
+
+  def __init__(self):
+    self.is_mute = HIGH
+    os.system('gpio mode 7 out')
+    os.system('gpio write 7 1')
+
   def play(self, filename, out='local', volume='-2000', background=True):
     if background:
       os.system("omxplayer -o {} --vol {} {} &".format(out, volume, filename))
@@ -12,3 +21,10 @@ class cAudio:
 
   def stop(self):
     os.system('sudo pkill omxplayer')
+  
+  def mute(self):
+    if self.is_mute == HIGH:
+      self.is_mute = LOW
+    else:
+      self.is_mute = HIGH
+    os.system(f'gpio write 7 {self.is_mute}')
